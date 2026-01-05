9bd7833f8eae370f3fca339650c93a4cd046c39e
Mauricio Soares <mauricio.massayuki@gmail.com>
2026-01-05 09:49:58 -0300

    added deep neural network example script

---
+++ neural_networks/deep_neural_network.py
@@ -0,0 +1,18 @@
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+# Define the architecture of the network
+model = Sequential()
+model.add(Dense(64, activation="relu", input_dim=100))
+model.add(Dense(64, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+# Compile the model
+model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+
+# # Train the model
+# model.fit(x_train, y_train, epochs=10, batch_size=32)
+
+# # Evaluate the model
+# loss, accuracy = model.evaluate(x_test, y_test, batch_size=32)
